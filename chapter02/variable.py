98a18f4ae9aec72d6941aaa838608d513d5531f2
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-18 21:48:12 +0700

    chapter02

---
+++ chapter02/variable.py
@@ -0,0 +1,13 @@
+print(float(99) + 100)
+i = 42
+print(type(i))
+f = float(i)
+print(f)
+print(type(f))
+
+nam = input("Who are you? ")
+print('Wellcome', nam)
+
+inp = input('Europe floor? ')
+usf = int(inp) + 1
+print('US floor', usf)
