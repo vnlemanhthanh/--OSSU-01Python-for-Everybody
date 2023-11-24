b5b261c9d860f650b6d5710f51097ec93125bdd6
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-24 10:26:35 +0700

    Exercise 5.1

---
+++ chapter05/exercise051.py
@@ -0,0 +1,15 @@
+num = 0
+tot = 0.0
+while True:
+    sval = input('Enter a number: ')
+    if sval == 'done':
+        break
+    try:
+        fval = float(sval)
+    except:
+        print('Invalid input')
+        continue
+    print(fval)
+    num += 1
+    tot += fval
+print(tot, num, tot/num)
