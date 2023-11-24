9e6a083c5f387f156d52325fc739717cbe035a2e
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-24 21:14:26 +0700

    close Autograder: Exercise 7.2 #17

---
+++ chapter07/exercise072.py
@@ -0,0 +1,24 @@
+#
+fname = input('Enter file name: ')
+# fname = 'mbox-short.txt'
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened: %s' % fname)
+    quit()
+
+string = 'X-DSPAM-Confidence:'
+count = 0
+tot = 0
+
+for line in fhand:
+    if not line.startswith(string):
+        continue
+    # print(line)
+    ipos = line.find(':')
+    num = line[ipos+1:]
+    num = float(num)
+    count += 1
+    tot += num
+
+print('Average spam confidence:', tot/count)
